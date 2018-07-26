f45d6f816293910fc88b08c98a1deedde18f8187
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 11:54:55 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: { scope: [:artist_name, :release_year] }
-  validates :boolean, inclusion: { in: [true, false]}
-  validates :release_year
+  validates :released, inclusion: { in: [true, false]}
+  validates :release_year, presence: true if: :released?
   validates :artist_name, presence: true
+
+  def released?
+    released == true
+  end
 end
