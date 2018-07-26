26a3a9e521ad0b0d4d6529fd024d4b504e2cb3a1
Marissa Zhong <mwz2101@columbia.edu>
2018-07-26 11:56:48 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,29 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
 
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    @author = Author.new(author_params)
+    if @author.save
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def author_params
+    params.permit(:name, :email, :phone_number)
+  end
 
 end
